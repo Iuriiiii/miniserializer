a3f05169815c4f6128645adc48893b7555486643
Iuriiiiiii <alex.casas@webtroniclabs.com>
2025-08-29 19:18:38 -0300

    fix: export more types

--- mod.ts
+++ mod.ts
@@ -17,12 +17,13 @@ import {
   serializeUint8Array,
   serializeURL,
 } from "./src/handlers/mod.ts";
-import type { DeserializeOptions, SerializeOptions } from "./src/interfaces/mod.ts";
-import type { DeserializeFunction, SerializerFunction } from "./src/types/mod.ts";
 import { assert } from "@std/assert";
+import type { DeserializeOptions, SerializeOptions } from "./src/interfaces/mod.ts";
+import type { AtLeastOneOf, DeserializeFunction, SerializerFunction } from "./src/types/mod.ts";
+import { SerializableClass } from "./src/abstractions/mod.ts";
 
-export { Database, isSerializableClass, ReadableStack, registerClass, WritableStack };
-export type { DeserializeFunction, DeserializeOptions, SerializeOptions, SerializerFunction };
+export { Database, isSerializableClass, ReadableStack, registerClass, SerializableClass, WritableStack };
+export type { AtLeastOneOf, DeserializeFunction, DeserializeOptions, SerializeOptions, SerializerFunction };
 
 const SERIALIZERS: SerializerFunction[] = [
   serializeDate,
